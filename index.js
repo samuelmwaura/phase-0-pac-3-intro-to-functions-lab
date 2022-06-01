21c35170dcbc31f8af47c28aa899587fb0671267
Samuel Mwaura <samuelmwaura081@gmail.com>
2022-06-01 18:43:22 +0300

    Completed the lab

---
+++ index.js
@@ -0,0 +1,27 @@
+function shout(word){
+return word.toUpperCase();
+}
+
+function whisper(word){
+    return word.toLowerCase();
+};
+
+function logShout(word){
+  console.log(`${word.toUpperCase()}`);
+}
+function logWhisper(word){
+  console.log(`${word.toLowerCase()}`);
+}
+
+function sayHiToHeadphonedRoommate(sentence){
+    switch(sentence){                 //Switch compares with strict comparison operator.
+       case sentence.toUpperCase():
+           return "YES INDEED!";
+       case sentence.toLowerCase():
+           return "I can't hear you!";
+       case "Let's have dinner together!":
+           return "I would love to!"
+    }
+}
+console.log(shout('mwaura'));//because it has a return value, It will log the return value of the function.
+console.log(whisper('MWAURA'));
